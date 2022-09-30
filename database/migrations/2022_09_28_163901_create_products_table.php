ee6bbb8e47fd137b2cb9583e19ad41bb3db4486b
Raul Smith <raul.n.smith@gmail.com>
2022-09-30 15:13:11 -0600

    Initializing Repo

---
+++ database/migrations/2022_09_28_163901_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id')->unique();
+            $table->string('sku');
+            $table->string('category');
+            $table->string('title');
+            $table->text('description');
+            $table->float('price', 8, 2);
+            $table->string('paypal_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
